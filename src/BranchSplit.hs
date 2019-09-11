803566cfe681ff764c1706e106fa659f8001bf74
JesseBalster <jessebalster@gmail.com>
2019-09-11 11:00:45 +0200

    Add while loop syntax

--- src/BranchSplit.hs
+++ src/BranchSplit.hs
@@ -8,7 +8,10 @@ splitBranch s@(Assume _ )   = [[s]]
 splitBranch s@(AAssign _ _ _)    = [[s]]
 splitBranch s@(Assign _ _)    = [[s]]
 splitBranch s@(Seq s1 s2)       = [ x ++ y  | x <-  splitBranch s1, y <- splitBranch s2 ]
-splitBranch s@(IfThenElse g s1 s2) = splitBranch s1 ++ splitBranch s2
+splitBranch s@(IfThenElse g s1 s2) = (map (\xs-> (Assume g) : xs) (splitBranch s1)) ++
+      (map (\xs-> (Assume (OpNeg g) : xs)) (splitBranch s2))
+-- Might be worthwhile to look into replacing ++ for something else
+splitBranch s@(While exp stmt) = (map (\xs-> (Assume exp) : xs ++ [Assume (OpNeg exp)]) (splitBranch stmt))
 splitBranch s = [[s]]
 
 {-
