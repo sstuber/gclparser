ec94ae3a23e3e05a15a1dd55d02f78dd0ad5a088
JesseBalster <jessebalster@gmail.com>
2019-10-01 15:09:43 +0200

    Remove todo

--- src/Main.hs
+++ src/Main.hs
@@ -18,9 +18,6 @@ uNFOLDLOOP = 1
 -- TODO Total number of inspected paths, and of these, the number of paths you manage to identify as unfeasible.
 {- TODO Consumed computation time: time spent on verification, time spent on identifying unfeasible paths, time spent on
     array assignment optimization (see below), and total used time. -}
-{- TODO Total size of the formulas that you have to verify. We’ll define size of a formula f to be the number of atoms in
-    f . An atom is a maximal expression that does not contain a boolean operator. For example the formula 1<x ∧ 0<x
-    has two atoms, and its size is 2. -}
 
 main :: IO ()
 main = do
